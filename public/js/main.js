aa3314526bd88d41edb39c3ac4a38ca3fabf629f
Dragos Ivan <dragos.ivan@reea.net>
2015-05-13 15:47:56 +0300

    Added registration view and functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,52 @@
 $(document).ready(function() {
+
+	// START GENERAL JS //
 	setTimeout(function() {
 		var windowHeight = $(window).height() - 200;	
 
 		if ($('.main-wrapper').height() < windowHeight) $('.main-wrapper').css('height', windowHeight);
 
-		
 		$('.slider-arrow').css('left', ($('.active-nav').width()/2 - 15)*$('.active-nav').data('nav'));
-
-		// var logosWidth = 0;
-
-		// jQuery.each($('.util-link img').width(), function(key, value) {
-		// 	console.log(value)
-		// })
-
-		// console.log(logosWidth)
-		// $('.util-link img').css('margin', '0 ' + $('.util-link').width() / 6)
 
 		$('.main-wrapper').css('visibility', 'visible');
 	}, 500)
+
+	$(window).on('resize', function() {
+		var windowHeight = $(window).height() - 200;	
+
+		if ($('.main-wrapper').height() < windowHeight) $('.main-wrapper').css('height', windowHeight);
+	})
+	// END GENERAL JS //
+
+
+
+	// START REGISTRATION PAGE JS //
+	$('.go-to-optional-registration').on('click', function() {
+		scrollToTop();
+		$('.registration-mandatory').fadeOut(500);
+		setTimeout(function(){
+			$('.registration-optional').fadeIn(500);
+		}, 500)
+	});
+
+	$('.go-to-mandatory-registration').on('click', function() {
+		scrollToTop();
+		$('.registration-optional').fadeOut(500);
+		setTimeout(function(){
+			$('.registration-mandatory').fadeIn(500);
+		}, 500)
+	});	
+
+	$('.submit-registration').on('click', function() {
+		$('.register-form').submit();
+	})
+	// END REGISTRATION PAGE JS//
 });
 
-$(window).on('load', function() {
-	$('.main-wrapper').css('visibility', 'visible');
-})
+var timeOut;
+function scrollToTop() {
+  if (document.body.scrollTop != 0 || document.documentElement.scrollTop != 0){
+    window.scrollBy(0, -50);
+    timeOut = setTimeout('scrollToTop()', 10);
+  } else clearTimeout(timeOut);
+}
